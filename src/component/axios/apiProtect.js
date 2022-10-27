1cc394dc7dbc937b55427a540fa0b2a217a0a76c
gary wallace <garyatgreatvacation4u@gmail.com>
2022-10-27 00:44:10 -0400

    removed proxy from packages

--- src/component/axios/apiProtect.js
+++ src/component/axios/apiProtect.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // import {useParams} from 'react-router-dom';
 const defaultURL = (process.env.NODE_ENV==="production") ? process.env.REACT_APP_URL : "http://localhost:8000/api";
 axios.defaults.baseURL =defaultURL;
-axios.defaults.baseURL ="http://localhost:8000/api";
+// axios.defaults.baseURL ="http://localhost:8000/api";
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.xsrfHeaderName = 'accept';
