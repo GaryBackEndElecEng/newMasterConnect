c31e4ed27f9deda7463daa9004a33ad8629299b6
gary wallace <garyatgreatvacation4u@gmail.com>
2023-01-25 13:31:57 -0500

    finished with product Array

--- src/component/design14/Product.js
+++ src/component/design14/Product.js
@@ -2,9 +2,8 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { GeneralContext } from '../../context/GeneralContextProvider';
 import { TokenAccessContext } from '../../context/TokenAccessProvider';
-import { useTheme } from '@mui/material/styles';
 import styled from 'styled-components';
-import { Stack, Card, CardMedia, Typography, Box, Fab, Grid, CardContent, Avatar, Container } from '@mui/material';
+import { Stack, Card, CardMedia, Typography, Fab, Grid, CardContent, Avatar, Container } from '@mui/material';
 import UserSignedInPurchaseBtn from '../utils/UserSignedInPurchaseBtn';
 import CoverPage from './CoverPage';
 import styles from './design14.module.css';
@@ -16,6 +15,8 @@ import PageFeedback from '../utils/PageFeedback';
 import products from './product.json';
 import sliderImg from './slider.json';
 import ModalContainer from '../utils/ModalContainer';
+import Included from '../utils/Included';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 
 
 const MainCover = styled.div`
@@ -32,10 +33,13 @@ const Product = () => {
     const pathname = location.pathname;
     const [getProducts, setGetProducts] = useState({ loaded: false, data: [] });
     const [start, setStart] = useState({ id: null, loaded: false });
-    const { setTitle, setStyleName,average,getPathLocation,pageRatings,staticImage, } = useContext(GeneralContext);
+    const { setTitle, setStyleName,average,getPathLocation,pageRatings,staticImage,getProductDesigns } = useContext(GeneralContext);
     const { paid } = useContext(TokenAccessContext);
     const [pageRatingHelmet, setPageRatingHelmet] = useState(null);
     const [showPurchaseBtn, setShowPurchaseBtn] = useState(false);
+    const [seeDetail, setSeeDetail] = useState({loaded:false,id:null});
+    const [detail, setDetail] = useState(false);
+    const [OBJ,setOBJ]=useState(null);
     const [keywords, setKeywords] = useState([]);
     const [summary, setSummary] = useState([]);
     const image=`${staticImage}/design14.png`;
@@ -46,7 +50,11 @@ const Product = () => {
             setPageRatingHelmet(pageRatings.data.filter(obj => (obj.page === pathname)))
                 }
         }
-    }, [pathname, pageRatings]);
+        if(getProductDesigns.loaded){
+        let getOBJ=getProductDesigns.data.filter(obj=>(obj.extra_kwargs==="/design14"))[0];
+        setOBJ(getOBJ);
+        }
+    }, [pathname, pageRatings,getProductDesigns]);
 
     useEffect(() => {
         let arr=[];
@@ -76,6 +84,26 @@ const Product = () => {
             setStart({ loaded: false, id: null });
         }
     }
+    const handleSeeDetail=(e,id)=>{
+        e.preventDefault();
+        if(!seeDetail.loaded){
+        setSeeDetail({loaded:true,id:id});
+        setDetail(false);
+        }else{
+            setSeeDetail({loaded:false,id:null})
+        }
+
+    }
+    const handleShowMe=(e)=>{
+        if(e ){
+            
+        }
+    }
+    const handleShowMeOut=(e)=>{
+        if(e){
+            
+        }
+    }
 
     return (
         <MainCover>
@@ -96,10 +124,31 @@ const Product = () => {
                     <Grid item xs={12} sm={6} md={4} key={`${obj.id}-product--${index}`}>
                         <Card elevation={10}
                             sx={{ position: "relative", display: "flex", justifyContent: "flex-start", alignItems: "center", flexDirection: "column" }}
+                            
                         >
                             <Avatar src={obj.logo} alt="www.masterconnect.ca"
-                                sx={{ width: "75px", height: "75px", boxShadow: "1px 1px 20px 3px black", alignSelf: "stretch", margin: "0.5rem" }}
+                                sx={{ width: "75px", height: "75px", boxShadow: "1px 1px 20px 3px black", alignSelf: "stretch", margin: "0.5rem",cursor:"pointer",position:"relative" }}
+                                onClick={(e)=>handleSeeDetail(e,obj.id)}
+                                className={ styles.showClickMe}
+                                onMouseOver={(e)=>handleShowMe(e)}
                             />
+                            {(seeDetail.loaded && seeDetail.id===obj.id) &&
+                            <Stack direction="column" sx={{justifyContent:"center",alignItems:"center",position:"absolute",top:"0%",left:"15%"}}>
+                                {!detail ? 
+                                <>
+                                <Typography component='h1' variant="h5">See detail</Typography>
+                                <Fab variant="extended" color="secondary" size="medium"
+                                onClick={()=>setDetail(true)}
+                                >
+                                    product  <ManageSearchIcon sx={{ml:1,color:"red"}}/>
+                                </Fab>
+                                </>
+                                :
+                                <Typography component='h1' variant="h5">Sends you to the detail page</Typography>
+                                
+                            }
+                            </Stack>
+                            }
                             <Stack direction="row" spacing={{ xs: 1, md: 2 }} sx={{ margin: "auto", width: "100%", overFlowX: "scroll", cursor: "pointer" }}
 
                                 onClick={(e) => handleSlide(e, obj.id)}
@@ -130,6 +179,7 @@ const Product = () => {
 
                 ))}
             </Grid>
+            <Included product={OBJ} staticImage={staticImage}/>
             <Container maxWidth="lg" sx={{ margin: "1rem auto" }}>
                 <Typography component="h1" variant="h4" sx={{ textAlign: "center", margin: "1rem auto" }}>Please comment - we aim to improve</Typography>
                 <PageFeedback />
